5938fff601cb684d8b705f9a509545088a641837
RafaelRiber <rafael@riber.ch>
2019-03-09 18:37:54 +0100

    Error mode recognition added. Created stub for read function.

--- projet.cc
+++ projet.cc
@@ -1,6 +1,25 @@
+#include <iostream>
 #include "simulation.h"
 
-int main(int argc, char const *argv[]) {
+using namespace std;
+
+void read(char *file_name);
+
+int main(int argc, char *argv[]) {
+
+  static bool errorMode = false;
+
+  if (argc > 1){
+    if (argv[1] == string("Error")){
+      errorMode = true;
+      read(argv[2]);
+    }
+    else read(argv[1]);
+  };
 
   return 0;
+}
+
+void read(char *file_name){
+  cout << "FILE READ NOT YET IMPLEMENTED" << endl;
 }
