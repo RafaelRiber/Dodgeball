fca959dafe73799fa10016e4c3d1e82ea4d95db7
RafaelRiber <rafael@riber.ch>
2019-03-10 14:54:59 +0100

    Started work on tools module

--- tools.cc
+++ tools.cc
@@ -1,1 +1,6 @@
 #include "tools.h"
+
+Point::Point(double x_in, double y_in){
+  x = x_in;
+  y = y_in;
+}
--- tools.h
+++ tools.h
@@ -1,6 +1,32 @@
 #ifndef TOOLS_H
 #define TOOLS_H
 
+class Point {
+public:
+  Point(double x_in, double y_in);
+private:
+  double x, y;
+};
 
+class Vector {
+public:
+
+private:
+  double x, y;
+};
+
+class Circle {
+public:
+
+private:
+  double center, radius;
+};
+
+class Square {
+public:
+
+private:
+
+};
 
 #endif
