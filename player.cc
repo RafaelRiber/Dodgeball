2e33da75a82cd4e48849b6617ffadd88431fff51
RafaelRiber <rafael@riber.ch>
2019-03-16 19:11:57 +0100

    Start work on player module

--- player.cc
+++ player.cc
@@ -1,2 +1,10 @@
+#include <vector>
 #include "player.h"
-#include "tools.h"
+
+static std::vector<Player> players;
+
+Player::Player(double x_in, double y_in, int nbt_in, int count_in)
+: coordinates(x_in, y_in){
+  nbt = nbt_in;
+  count = count_in;
+}
--- player.h
+++ player.h
@@ -1,6 +1,17 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include "tools.h"
+
+class Player {
+public:
+  Player(double x_in, double y_in, int nbt_in, int count_in);
+
+private:
+  Point coordinates;
+  int nbt, count;
+};
+
 
 
 #endif
