efdf36d18ff4dbfc65d55c6024ce4100018bbb80
RafaelRiber <rafael@riber.ch>
2019-03-17 21:17:40 +0100

    Added function to add players to list of players

--- player.cc
+++ player.cc
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 #include "player.h"
 
 static std::vector<Player> players;
@@ -7,4 +8,12 @@ Player::Player(double x_in, double y_in, int nbt_in, int count_in)
 : coordinates(x_in, y_in){
   nbt = nbt_in;
   count = count_in;
+}
+
+void Player::add(){
+  players.push_back(*this);
+}
+
+void playerPrintSize(){
+  std::cout << players.size() << std::endl;
 }
