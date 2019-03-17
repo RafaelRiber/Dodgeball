7cbe4067963873117d55a80cafe194f1cae818bc
RafaelRiber <rafael@riber.ch>
2019-03-17 22:24:25 +0100

    decode line doesn't need to be public

--- simulation.h
+++ simulation.h
@@ -9,6 +9,8 @@ class Simulation {
 public:
   void read(char *file_name);       //Normal Read
   void read_error(char *file_name); //Error mode read
+
+private:
   void decodeLine(std::string line);
 };
 
