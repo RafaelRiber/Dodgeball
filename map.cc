2dabad8a57796c56fe7d15b8b41ac063fa0a7c85
RafaelRiber <rafael@riber.ch>
2019-03-17 19:55:53 +0100

    Initial data structure and constructor

--- map.cc
+++ map.cc
@@ -1,2 +1,12 @@
 #include "map.h"
 #include "tools.h"
+
+
+Map::Map(int nbCell){
+  obstacleMap.resize(nbCell);
+  for (int i = 0; i < nbCell; ++i) obstacleMap[i].resize(nbCell);
+
+  for (auto &v: obstacleMap) {
+    std::fill(v.begin(), v.end(), 0);
+  }
+};
--- map.h
+++ map.h
@@ -1,11 +1,20 @@
 #ifndef MAP_H
 #define MAP_H
 
+#include <vector>
+
+// Size of map is nbCell x nbCell
+// and must be minimum MIN_CELL (3x3) and maximum [-DIM_MAX, DIM_MAX] (800x800)
+
 class Map {
 public:
+  Map(int nbCell);
+  // setObstacle(int line, int column);
+  // setEmpty(int line, int column);
 
 private:
-  int nbCell;
-}
+  int nbCell;  //Must be at least MIN_CELL
+  std::vector<std::vector<int> > obstacleMap;
+};
 
 #endif
