ab4ce385e4ae294044a010cd692b4e8b03ee0ebd
RafaelRiber <rafael@riber.ch>
2019-03-18 19:21:04 +0100

    Added player coordinates getter

--- player.cc
+++ player.cc
@@ -1,8 +1,13 @@
 #include <vector>
 #include "player.h"
+#include "define.h"
 
 Player::Player(double x_in, double y_in, int nbt_in, int count_in)
 : coordinates(x_in, y_in){
   nbt = nbt_in;
   count = count_in;
+}
+
+Point Player::getPlayerCoordinates() {
+  return coordinates;
 }
