7e94cfb24ca620c40334f16c7ed7b978160a6937
RafaelRiber <rafael@riber.ch>
2019-03-19 19:38:44 +0100

    Changed variable names

--- tools.cc
+++ tools.cc
@@ -15,8 +15,8 @@ void Point::getCoordinates(double &x_out, double &y_out) const {
   y_out = this->y;
 }
 
-bool Point::inBoundary(double boundX, double boundY){
-  if (x < -boundX or x > boundX or y < -boundY or y > boundY){
+bool Point::inBoundary(double boundaryX, double boundaryY){
+  if (x < -boundaryX or x > boundaryX or y < -boundaryY or y > boundaryY){
     return false;
   }
   else{
