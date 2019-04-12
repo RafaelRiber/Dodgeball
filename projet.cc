6ed214c686214a4aa863eb09e93805cdf2a6b016
RafaelRiber <rafael@riber.ch>
2019-04-12 12:04:49 +0200

    Cleanup

--- projet.cc
+++ projet.cc
@@ -10,21 +10,19 @@
 #include "error.h"
 #include "gui.h"
 
-#define MODE_OR_FILE_NAME_IF_GUI_MODE     1
-#define FILE_NAME_OR_NOTHING_IF_GUI_MODE  2
+#define FIRST_ARG   1
+#define SECOND_ARG  2
 
 int main(int argc, char *argv[]) {
 
   int mode(NOFILE);
 
-  //simulation.read("C:/Users/Valentin/Documents/GitHub/Dodgeball/E02.txt"); exit(0); //debug
-
   if (argc > 1){
-    if (strcmp(argv[MODE_OR_FILE_NAME_IF_GUI_MODE], "Error") == 0){
+    if (strcmp(argv[FIRST_ARG], "Error") == 0){
       mode = ERROR;
       Simulation simulation;
-      simulation.read_error(argv[FILE_NAME_OR_NOTHING_IF_GUI_MODE]);
-    }else if(strcmp(argv[MODE_OR_FILE_NAME_IF_GUI_MODE], "Step") == 0){
+      simulation.read_error(argv[SECOND_ARG]);
+    }else if(strcmp(argv[FIRST_ARG], "Step") == 0){
       mode = STEP;
       Simulation simulation;
       std::cout<<"Step mode not yet implemented"<<std::endl;
